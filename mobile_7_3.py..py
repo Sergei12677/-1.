e0945cf83edac7e7c620d15cfc1a261189e5af86
Sergei12677 <gorod_26@icloud.com>
2024-10-13 15:46:13 +0500

    Add files via upload

---
+++ mobile_7_3.py..py
@@ -0,0 +1,8 @@
+from traceback import print_exc
+
+grades = [[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
+students = {'Johnny','bildo','Steve','Khendrik','Aaron'}
+phone_book ={"Johnny":20,"bildo":(3,667),"Steve":4,'Khendrik':(2,25),'Aaron':4}
+print(phone_book)
+
+
